72cc01b3138fd4d23d8f18e538d6631792f50175
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-06-29 23:04:15 +0530

    Added css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
@@ -7,25 +6,25 @@ class Calculator extends React.Component {
       <div className="calculator-div">
         <input className="displayNum" type="text" id="result" placeholder="0" disabled />
         <div className="calculatorButtons">
-          <button className="color-first" type="submit">AC</button>
-          <button className="color-first" type="submit">+/-</button>
-          <button className="color-first" type="submit">%</button>
-          <button className="color-first" type="submit">/</button>
-          <button className="color-second" type="submit">7</button>
-          <button className="color-second" type="submit">8</button>
-          <button className="color-second" type="submit">9</button>
-          <button className="color-first" type="submit">*</button>
-          <button className="color-second" type="submit">4</button>
-          <button className="color-second" type="submit">5</button>
-          <button className="color-second" type="submit">6</button>
-          <button className="color-first" type="submit">-</button>
-          <button className="color-second" type="submit">1</button>
-          <button className="color-second" type="submit">2</button>
-          <button className="color-second" type="submit">3</button>
-          <button className="color-first" type="submit">+</button>
-          <button className="color-second zero" type="submit">0</button>
-          <button className="color-second" type="submit">.</button>
-          <button className="color-first" type="submit">=</button>
+          <button className="color-f" type="submit">AC</button>
+          <button className="color-f" type="submit">+/-</button>
+          <button className="color-f" type="submit">%</button>
+          <button className="color-f" type="submit">/</button>
+          <button className="color-s" type="submit">7</button>
+          <button className="color-s" type="submit">8</button>
+          <button className="color-s" type="submit">9</button>
+          <button className="color-f" type="submit">*</button>
+          <button className="color-s" type="submit">4</button>
+          <button className="color-s" type="submit">5</button>
+          <button className="color-s" type="submit">6</button>
+          <button className="color-f" type="submit">-</button>
+          <button className="color-s" type="submit">1</button>
+          <button className="color-s" type="submit">2</button>
+          <button className="color-s" type="submit">3</button>
+          <button className="color-f" type="submit">+</button>
+          <button className="color-s zero" type="submit">0</button>
+          <button className="color-s" type="submit">.</button>
+          <button className="color-f" type="submit">=</button>
         </div>
       </div>
     );
